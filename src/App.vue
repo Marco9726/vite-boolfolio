af3153d9e46b5a2e7fd36e03eb64c159e9904d30
Marco 9726 <marcosarra9@gmail.com>
2023-03-11 18:49:55 +0100

    creazione componente vue ProjectMain

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
-<script setup>
-
+<script>
+	import ProjectMain from './components/ProjectMain.vue';
+	export default{
+		components:{
+			ProjectMain
+		}
+	}
 </script>
 
 <template>
---
+++ src/components/ProjectMain.vue
@@ -0,0 +1,32 @@
+<script>
+	export default{
+		name: 'ProjectMain',
+		data(){
+			return{
+				projects: [],
+				loading: true,
+				Uri: 'http://127.0.0.1:8000/'
+			}
+		},
+		methods:{
+			getPosts(){
+				this.loading = true;
+				axios.get(`${this.Uri}/api/projects`).then(response => {
+					if(response.data.success){
+						this.projects = response.data.projects
+						this.loading = false;
+					}
+				})
+			}
+		}
+
+	}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
