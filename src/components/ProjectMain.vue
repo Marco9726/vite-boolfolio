b0491e5a0cd43665c85c7da6853bf6593cc22a90
Marco 9726 <marcosarra9@gmail.com>
2023-03-12 16:57:50 +0100

    aggiornamento stile template ProjectMain

--- src/components/ProjectMain.vue
+++ src/components/ProjectMain.vue
@@ -6,19 +6,22 @@
 			return{
 				projects: [],
 				loading: true,
-				Uri: 'http://127.0.0.1:8000/'
+				Uri: 'http://127.0.0.1:8000'
 			}
 		},
 		methods:{
 			getPosts(){
 				this.loading = true;
-				axios.get(`${this.Uri}/api/projects`).then(response => {
+				axios.get(`${this.Uri}/api/projects`).then((response) => {
 					if(response.data.success){
-						this.projects = response.data.projects
+						this.projects = response.data.results
 						this.loading = false;
 					}
 				})
 			}
+		},
+		mounted(){
+			this.getPosts();
 		}
 
 	}
@@ -32,14 +35,14 @@
 					<h2 class="text-center">Boolpress</h2>
 				</div>
 				<div class="col-12">
-					<div v-if="this.loading = true" id="loader">
+					<div v-if="this.loading == true" id="loader">
 						in caricamento
 					</div>
-					<div v-else class="d-flex justify-content-center">
+					<div v-else class="d-flex justify-content-between flex-wrap">
 						<div class="card my-3" v-for="project in projects" :key="project.id">
 							<div class="card-body">
-								<div class="card-img-top">
-									<img :src="project.cover_image != null ? project.cover_image : 'https://i1.wp.com/potafiori.com/wp-content/uploads/2020/04/placeholder.png?ssl=1'" :alt="project.title" class="img-fluid">
+								<div class="card-img-top mb-2">
+									<img :src="project.cover_image != null ? project.cover_image : 'https://avatars.mds.yandex.net/i?id=8d48f296e8bb9995f9401bfaf34c93637815f6a2-8219873-images-thumbs&n=13'" :alt="project.title" class="img-fluid">
 								</div>
 								<div class="card-title">
 									<h5>
@@ -66,4 +69,7 @@
 
 <style lang="scss" scoped>
 
+	.card{
+		width: calc(100% / 3 - 1rem);
+	}
 </style>
